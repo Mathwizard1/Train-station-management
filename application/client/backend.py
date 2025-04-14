56378bbc9ec7c0e82b73e77fc28d6a9b0942ba56
Mathwizard1 <anshurup.gupta@gmail.com>
2025-04-14 06:32:28 +0530

    basic home

--- application/client/backend.py
+++ application/client/backend.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-#from application.server.databaseConnector import debug_print
+# from application.server.databaseConnector import DatabaseConnector
 from flask import session
 
 import secrets
@@ -25,20 +25,21 @@ def handle_login():
 
 @app.route('/login/<login_mode>', methods=['GET', 'POST'])
 def login(login_mode):
-    print(login_mode)
+
+
 
     if('user_id' in session):
         return redirect(url_for('home'))
 
     if(login_mode == "signin"):
         if request.method == 'POST':
-            username = request.form.get('username')
+            session['username'] = request.form.get('username')
             password = request.form.get('password')
 
-            if username != '' and password != '':
+            if session['username'] != '' and password != '':
                 # TODO sql check
 
-                # session['user_id'] = username
+                #session['user_id'] = userid
 
                 return redirect(url_for('home'))
             else:
@@ -46,6 +47,8 @@ def login(login_mode):
     elif(login_mode == "signup"):
         if request.method == 'POST':
             username = request.form.get('username')
+            age = request.form.get('age', None, type= int)
+            gender = request.form.get('gender')
             password = request.form.get('password')
             repassword = request.form.get('repassword')
 
@@ -53,21 +56,50 @@ def login(login_mode):
                 # TODO sql check
 
                 # session['user_id'] = username
+                session['username'] = username
 
                 return redirect(url_for('home'))
             else:
                 return render_template('login.html', login_mode= login_mode, error_message="Invalid credentials")
     return render_template('login.html', login_mode= login_mode)
 
-@app.route('/home')
+@app.route('/home', methods= ['GET', 'POST'])
 def home():
-    #debug_print()
-    return render_template('home.html')
+    if('username' in session):
+        form_fields = [
+        {'name': 'name', 'label': 'Name', 'type': 'text'},
+        {'name': 'email', 'label': 'Email', 'type': 'email'},
+        {'name': 'age', 'label': 'Age', 'type': 'number'},
+        ]
+        train_headers = ["Tid", 'Train', 'Arv. Station', 'Arv. Time', 'Dep. Station', 'Dep. Time']
+        train_data = [
+            ('Train', 'Arv. Station', 'Arv. Time', 'Dep. Station', 'Dep. Time'),
+            ('Train', 'Arv. Station', 'Arv. Time', 'Dep. Station', 'Dep. Time'),
+            ('Train', 'Arv. Station', 'Arv. Time', 'Dep. Station', 'Dep. Time')
+        ]
 
-# For reset
+        # if request.method == 'POST':
+        #     form_data = request.form.to_dict()
+        #     print("Form Data:", form_data)
+        #     #  Here you would process the form data (e.g., save to a database)
+        #     table_data.append(form_data) # just add to table_data for display
+        #     return render_template('home.html', 
+        #             form_fields=form_fields, 
+        #             table_headers=table_headers, 
+        #             table_data=table_data)
+
+        return render_template('home.html', 
+                    form_fields=form_fields, 
+                    table_headers= train_headers, 
+                    table_data=train_data,
+                    selected_row = 1)
+    return logout()
+
+# For reset session
+@app.route('/logout')
 def logout():
     session.clear()
-    redirect(url_for('/'))
+    return redirect(url_for('login', login_mode= 'signin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
